b7fa2f2ce89babca3eb936e47fb01032f4b02a53
wndaiga <w.ndaiga@gmail.com>
2018-07-14 12:07:29 +0100

    naive lawn algorithm implemented

--- controllers/eyebot_lawn/eyebot_lawn.cpp
+++ controllers/eyebot_lawn/eyebot_lawn.cpp
@@ -8,16 +8,37 @@
 /****************************************/
 /****************************************/
 
-/* Altitude to Lawn to move along the Lawn */
-static const Real ALTITUDE = 3.0f;
+/* Altitude to Lawn to move along the lawn */
+static const Real ALTITUDE = 0.1f;
+
+/* Distance to wall to move along the lawn at */
+static const Real REACH = 3.0f;
+
+/* Distance to move horizontally along the lawn length at */
+static const Real HORIZONTAL_STEP = 0.05f;
+
+/* Distance to move vertically along the lawn length at */
+static const Real VERTICAL_STEP = 0.05f;
+
+/* Tolerance for the distance to a target point to decide to do something else */
+static const Real PROXIMITY_TOLERANCE = 0.01f;
+
+/* How many points the robot traverses to move horizontally along the lawn */
+static const UInt32 LAWN_HORIZONTAL_WAYPOINTS = 50;
+
+/* How many points the robot traverses to move horizontally along the lawn */
+static const UInt32 LAWN_VERTICAL_WAYPOINTS = 5;
+
+/* Direction in which the robot traverses to move horizontally along the lawn */
+Real LAWN_DIRECTION = 1.0f;
 
 /****************************************/
 /****************************************/
 
 CEyeBotLawn::CEyeBotLawn() :
-   m_pcPosAct(NULL),
-   m_pcPosSens(NULL),
-   m_pcRABSens(NULL) {}
+    m_pcPosAct(NULL),
+    m_pcPosSens(NULL),
+    m_pcRABSens(NULL) {}
 
 /****************************************/
 /****************************************/
@@ -45,13 +66,13 @@ void CEyeBotLawn::Init(TConfigurationNode& t_node) {
     * list a device in the XML and then you request it here, an error
     * occurs.
     */
-   m_pcPosAct  = GetActuator<CCI_QuadRotorPositionActuator>("quadrotor_position");
-   m_pcPosSens = GetSensor  <CCI_PositioningSensor        >("positioning"       );
-   m_pcRABSens = GetSensor  <CCI_RangeAndBearingSensor    >("range_and_bearing" );
+    m_pcPosAct  = GetActuator<CCI_QuadRotorPositionActuator>("quadrotor_position");
+    m_pcPosSens = GetSensor  <CCI_PositioningSensor        >("positioning"       );
+    m_pcRABSens = GetSensor  <CCI_RangeAndBearingSensor    >("range_and_bearing" );
    /*
     * Initialize the state variables of the behavior
     */
-   Reset();
+    Reset();
 }
 
 /****************************************/
@@ -59,18 +80,36 @@ void CEyeBotLawn::Init(TConfigurationNode& t_node) {
 
 void CEyeBotLawn::ControlStep() {
    /* Get RAB message, if any */
-   RLOG << "Message received: ";
-   if(! m_pcRABSens->GetReadings().empty()) {
-      m_psFBMsg = &(m_pcRABSens->GetReadings()[0]);
-      LOG << *reinterpret_cast<const UInt64*>(m_psFBMsg->Data.ToCArray());
-   }
-   else {
-      m_psFBMsg = NULL;
-      LOG << "none";
-   }
-   LOG << std::endl;
-   /* Execute state logic */
-   TakeOff();
+    RLOG << "Message received: ";
+    if(! m_pcRABSens->GetReadings().empty()) {
+        m_psFBMsg = &(m_pcRABSens->GetReadings()[0]);
+        LOG << *reinterpret_cast<const UInt64*>(m_psFBMsg->Data.ToCArray());
+    }
+    else {
+        m_psFBMsg = NULL;
+        LOG << "none";
+    }
+    LOG << std::endl;
+    /* Execute state logic */
+    switch(m_eState) {
+        case STATE_START:
+            TakeOff();
+            break;
+        case STATE_TAKE_OFF:
+            TakeOff();
+            break;
+        case STATE_MOVE_HORIZONTALLY:
+            MoveHorizontally();
+            break;
+        case STATE_MOVE_VERTICALLY:
+            MoveVertically();
+            break;
+        case STATE_LAND:
+            Land();
+            break;
+        default:
+            LOGERR << "[BUG] Unknown robot state: " << m_eState << std::endl;
+    }
    /* Write debug information */
    RLOG << "Current state: " << m_eState << std::endl;
    RLOG << "Target pos: " << m_cTargetPos << std::endl;
@@ -80,33 +119,84 @@ void CEyeBotLawn::ControlStep() {
 /****************************************/
 
 void CEyeBotLawn::Reset() {
-   /* Start the behavior */
-   m_eState = STATE_START;
-   /* No message received */
-   m_psFBMsg = NULL;
+    /* Start the behavior */
+    m_eState = STATE_START;
+    /* No message received */
+    m_psFBMsg = NULL;
 }
 
 /****************************************/
 /****************************************/
 
 void CEyeBotLawn::TakeOff() {
-    /* State initialization */
-    m_eState = STATE_TAKE_OFF;
-    m_cTargetPos = m_pcPosSens->GetReading().Position + CVector3(0.0f, 0.0f, ALTITUDE);
-    m_pcPosAct->SetAbsolutePosition(m_cTargetPos);
+    if(m_eState != STATE_TAKE_OFF) {
+        /* State initialization */
+        m_eState = STATE_TAKE_OFF;
+        m_cTargetPos = m_pcPosSens->GetReading().Position + CVector3(0.0f, REACH, ALTITUDE);
+        m_pcPosAct->SetAbsolutePosition(m_cTargetPos);
+    } else {
+        if(Distance(m_cTargetPos, m_pcPosSens->GetReading().Position) < PROXIMITY_TOLERANCE) {
+            /* State transition */
+            MoveHorizontally();
+        }
+    }
 }
 
 /****************************************/
 /****************************************/
 
 void CEyeBotLawn::Land() {
-   if(m_eState != STATE_LAND) {
-      /* State initialization */
-      m_eState = STATE_LAND;
-      m_cTargetPos = m_pcPosSens->GetReading().Position;
-      m_cTargetPos.SetZ(0.0f);
-      m_pcPosAct->SetAbsolutePosition(m_cTargetPos);
-   }
+    if(m_eState != STATE_LAND) {
+        /* State initialization */
+        m_eState = STATE_LAND;
+        m_cTargetPos = m_pcPosSens->GetReading().Position;
+        m_cTargetPos.SetZ(0.0f);
+        m_pcPosAct->SetAbsolutePosition(m_cTargetPos);
+    }
+}
+
+/****************************************/
+/****************************************/
+
+void CEyeBotLawn::MoveHorizontally() {
+    if(m_eState != STATE_MOVE_HORIZONTALLY) {
+        /* State initialization */
+        m_eState = STATE_MOVE_HORIZONTALLY;
+        LAWN_DIRECTION = LAWN_DIRECTION * -1.0;
+    } else {
+        if(m_unWaypoint >= LAWN_HORIZONTAL_WAYPOINTS) {
+            /* State transition */
+            m_unWaypoint = 0;
+            MoveVertically();
+        } else {
+            /* State logic */
+            m_cTargetPos = m_pcPosSens->GetReading().Position + CVector3((HORIZONTAL_STEP)*LAWN_DIRECTION, 0.0f, 0.0f);
+            m_pcPosAct->SetAbsolutePosition(m_cTargetPos);
+            ++m_unWaypoint;
+        }
+    }
+}
+
+/****************************************/
+/****************************************/
+
+void CEyeBotLawn::MoveVertically() {
+    if(m_eState != STATE_MOVE_VERTICALLY) {
+        /* State initialization */
+        m_eState = STATE_MOVE_VERTICALLY;
+    } else {
+        if(m_unWaypoint >= LAWN_VERTICAL_WAYPOINTS) {
+            /* State transition */
+            m_unWaypoint = 0;
+            MoveHorizontally();
+        } else {
+            /* State logic */
+            m_cTargetPos = m_pcPosSens->GetReading().Position + CVector3(0.0f, 0.0f, VERTICAL_STEP);
+            m_pcPosAct->SetAbsolutePosition(m_cTargetPos);
+            ++m_unWaypoint;
+        }
+    }
+    
 }
 
 /****************************************/
--- controllers/eyebot_lawn/eyebot_lawn.h
+++ controllers/eyebot_lawn/eyebot_lawn.h
@@ -26,79 +26,91 @@ class CEyeBotLawn : public CCI_Controller {
 
 public:
 
-   /* Class constructor. */
-   CEyeBotLawn();
+    /* Class constructor. */
+    CEyeBotLawn();
 
-   /* Class destructor. */
-   virtual ~CEyeBotLawn() {}
+    /* Class destructor. */
+    virtual ~CEyeBotLawn() {}
 
-   /*
+    /*
     * This function initializes the controller.
     * The 't_node' variable points to the <parameters> section in the XML
     * file in the <controllers><eyebot_lawn_controller> section.
     */
-   virtual void Init(TConfigurationNode& t_node);
+    virtual void Init(TConfigurationNode& t_node);
 
-   /*
+    /*
     * This function is called once every time step.
     * The length of the time step is set in the XML file.
     */
-   virtual void ControlStep();
+    virtual void ControlStep();
 
-   /*
+    /*
     * This function resets the controller to its state right after the
     * Init().
     * It is called when you press the reset button in the GUI.
     */
-   virtual void Reset();
+    virtual void Reset();
 
-   /*
+    /*
     * Called to cleanup what done by Init() when the experiment finishes.
     * In this example controller there is no need for clean anything up,
     * so the function could have been omitted. It's here just for
     * completeness.
     */
-   virtual void Destroy() {}
+    virtual void Destroy() {}
 
 private:
 
-   /*
+    /*
     * Takes off the robot.
     */
-   void TakeOff();
+    void TakeOff();
 
-   /*
+    /*
+    * Moves the robot horizontally along the lawn path.
+    */
+    void MoveHorizontally();
+
+    /*
+    * Moves the robot vertically along the lawn path.
+    */
+    void MoveVertically();
+
+    /*
     * Lands the robot.
     */
-   void Land();
+    void Land();
 
 private:
 
-   /* Current robot state */
-   enum EState {
-      STATE_START = 0,
-      STATE_TAKE_OFF,
-      STATE_LAND
-   };
+    /* Current robot state */
+    enum EState {
+        STATE_START = 0,
+        STATE_TAKE_OFF,
+        STATE_MOVE_HORIZONTALLY,
+        STATE_MOVE_VERTICALLY,
+        STATE_LAND
+    };
 
 private:
 
-   /* Pointer to the quadrotor position actuator */
-   CCI_QuadRotorPositionActuator* m_pcPosAct;
-   /* Pointer to the positioning sensor */
-   CCI_PositioningSensor* m_pcPosSens;
-   /* Pointer to the range-and-bearing sensor */
-   CCI_RangeAndBearingSensor* m_pcRABSens;
+    /* Pointer to the quadrotor position actuator */
+    CCI_QuadRotorPositionActuator* m_pcPosAct;
+    /* Pointer to the positioning sensor */
+    CCI_PositioningSensor* m_pcPosSens;
+    /* Pointer to the range-and-bearing sensor */
+    CCI_RangeAndBearingSensor* m_pcRABSens;
 
-   /* Current robot state */
-   EState m_eState;
-   /* Current target position */
-   CVector3 m_cTargetPos;
-   /* Used to move the robot along the lawn */
-   UInt32 m_unWaypoint;
+    /* Current robot state */
+    EState m_eState;
+    /* Current target position */
+    CVector3 m_cTargetPos;
+    /* Used to move the robot along the lawn */
+    UInt32 m_unWaypoint;
 
-   /* Contains the message received from the foot-bot */
-   const CCI_RangeAndBearingSensor::SPacket* m_psFBMsg;
+    /* Contains the message received from the foot-bot */
+    const CCI_RangeAndBearingSensor::SPacket* m_psFBMsg;
 };
 
 #endif
