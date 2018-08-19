d3f14cee9d0bcde36fe6fdd3f17fd472d671c8a4
wndaiga <w.ndaiga@gmail.com>
2018-08-19 21:16:10 +0100

    initial xml setting with run.sh script

--- run.sh
+++ run.sh
@@ -1,53 +1,169 @@
 #!/usr/bin/env bash
 
-if [ ! -d build ]; then
-    sudo mkdir build
-fi
+PROJDIR=${PWD}
+EXPFILE="main"
+ALGOS=()
+EXPSEEDS=()
+TARGETNUMS=()
+TARGETTHRESHS=()
+CAMPOS=('-4.59862,0,4.59862' '-1.22369,0,9.18125' '-0.0457076,-5.0651,5.79638' '-0.205796,-10.9602,1.62526')
+CAMLOOK=('-3.80581,0,3.98914' '-1.1384,-0.000852933,8.1849' '-0.0353751,-4.56833,4.92856' '-0.195418,-9.99895,1.90085')
+CAMFOCAL=('20' '20' '20' '20')
+VIZ=0
 
-while getopts se:t: opt; do
+function build_project {
+    # Build and make the project
+    echo "Building and making project..."
+
+    if [ ! -d build ]; then
+        echo "Creating build directory..."
+        sudo mkdir build
+    fi
+
+    cd ${PROJDIR} &&
+    cd build &&
+    cmake .. &&
+    make
+
+    echo "Done!"
+}
+
+function run_experiment {
+    # Build, make and run project experiment
+    echo "Running ${EXPFILE} experiment..."
+    cd ${PROJDIR} &&
+    argos3 -l exp_info.log -e exp_err.log -c experiments/${EXPFILE}.argos
+    echo "Done!"
+}
+
+while getopts a:be:n:s:t:V opt; do
     case "$opt" in
-        s)
-            # Build and make the project
-            echo "Building and making project" >&2
-            cd build &&
-            cmake .. &&
-            make
+        a)
+            ALGOS=(${OPTARG})
+        ;;
+        b)
+            build_project
+            exit 0
         ;;
         e)
-            # Build, make and run project experiment
-            echo "Setting up to run $OPTARG experiment" >&2
-            cd build &&
-            cmake .. &&
-            make &&
-            echo "Running $OPTARG experiment" >&2 &&
-            cd .. &&
-            argos3 -c experiments/$OPTARG.argos
+            EXPFILE=${OPTARG}
+        ;;
+        n)
+            TARGETNUMS=(${OPTARG})
+        ;;
+        s)
+            EXPSEEDS=(${OPTARG})
         ;;
         t)
-            #Run multiple trials with passed seed values on main experiment
-            echo "Starting experiments with seeds: $OPTARG" >&2
-
-            seeds=(${OPTARG})
-            algos=("pso" "aco" "lawn")
-            targetnum=50
-            targetthresh=0.85
-
-            snum=${#seeds[*]}
-            anum=${#algos[*]}
-
-            for (( a=0; a<anum; a++ ))
-            do
-                echo "Beginning ${algos[a]} trial number ${a}..." >&2
-                for (( s=0; s<snum; s++))
-                do
-                    profile="profile_${algos[a]}_${s}.log"
-                    xmlstarlet ed -L -u 'argos-configuration/framework/experiment/@random_seed' -v ${seeds[s]} experiments/main.argos &&
-                    xmlstarlet ed -L -u 'argos-configuration/framework/profiling/@file' -v ${profile} experiments/main.argos &&
-                    xmlstarlet ed -L -u 'argos-configuration/controllers/main_controller/params/experiment/@name' -v ${algos[a]} experiments/main.argos &&
-                    xmlstarlet ed -L -u 'argos-configuration/controllers/main_controller/params/experiment/@target' -v ${targetthresh} experiments/main.argos &&
-                    xmlstarlet ed -L -u 'argos-configuration/arena/distribute[1]/entity/@quantity' -v ${targetnum} experiments/main.argos &&
-                    argos3 -c experiments/main.argos
-                done
-            done
+            TARGETTHRESHS=(${OPTARG})
+        ;;
+        V)
+            VIZ=1
     esac
+done
+
+ssize=${#EXPSEEDS[*]}
+asize=${#ALGOS[*]}
+tsize=${#TARGETNUMS[*]}
+threshsize=${#TARGETTHRESHS[*]}
+camsize=${#CAMPOS[*]}
+
+if [ ${asize} == 0 ]
+then
+    echo "No algorithms passed. Exiting!"
+    exit 1
+else
+    for algo in ${ALGOS[*]}
+    do
+        if [ "${algo}" != "pso" -a "${algo}" != "aco" -a "${algo}" != "lawn" ]
+        then
+            echo "Invalid algorithm passed: only pso, aco or lawn. Exiting!"
+            exit 2
+        fi
+    done
+fi
+
+if [ ${ssize} == 0 ]
+then
+    echo "No argos seeds passed. Exiting!"
+    exit 3
+fi
+
+if [ ${tsize} == 0 ]
+then
+    echo "No target numbers passed. Exiting!"
+    exit 4
+fi
+
+if [ ${threshsize} == 0 ]
+then
+    echo "No target thresholds passed. Exiting!"
+    exit 6
+else
+    for thresh in ${TARGETTHRESHS[*]}
+    do
+        if (( $(echo "${thresh} > 1.0" | bc -l) == 1 ))
+        then
+            echo "Invalid threshold value passed. Exiting!"
+            exit 5
+        fi
+    done
+fi
+
+build_project
+cd ${PROJDIR}
+
+for a in ${ALGOS[*]}
+do
+    echo -n "Beginning ${a} trial with "
+    trial=0
+    for s in ${EXPSEEDS[*]}
+    do
+        echo -n "( seed = ${s} )"
+        trial=0
+        for n in ${TARGETNUMS[*]}
+        do
+            echo -n "( targets = ${n} )"
+
+            if [ ! -d "data/${n}" ]; then
+                sudo mkdir data/${n}
+                echo -n " ( created data target directory )"
+            fi
+            trial=0
+            for t in ${TARGETTHRESHS[*]}
+            do
+                echo "( threshold = ${t} )"
+                profile="profile_${a}_${trial}_${s}.log"
+
+                xmlstarlet ed -L -u 'argos-configuration/framework/experiment/@random_seed' -v ${s} experiments/${EXPFILE}.argos &&
+                xmlstarlet ed -L -u 'argos-configuration/framework/profiling/@file' -v data/${n}/${profile} experiments/${EXPFILE}.argos &&
+                xmlstarlet ed -L -u 'argos-configuration/controllers/main_controller/params/experiment/@name' -v ${a} experiments/${EXPFILE}.argos &&
+                xmlstarlet ed -L -u 'argos-configuration/controllers/main_controller/params/experiment/@target' -v ${t} experiments/${EXPFILE}.argos &&
+                xmlstarlet ed -L -u 'argos-configuration/arena/distribute[1]/entity/@quantity' -v ${n} experiments/${EXPFILE}.argos
+
+                xmlstarlet ed -L -d 'argos-configuration/visualization' experiments/${EXPFILE}.argos &&
+
+                if [ ${VIZ} == 0 ]
+                then
+                    xmlstarlet ed -L -s 'argos-configuration' -t elem -n visualization -v '' experiments/${EXPFILE}.argos
+                else
+                    xmlstarlet ed -L -s 'argos-configuration' -t elem -n visualization -v '' experiments/${EXPFILE}.argos &&
+                    xmlstarlet ed -L -s 'argos-configuration/visualization' -t elem -n qt-opengl -v '' experiments/${EXPFILE}.argos &&
+                    xmlstarlet ed -L -s 'argos-configuration/visualization/qt-opengl' -t elem -n camera -v '' experiments/${EXPFILE}.argos &&
+
+                    for (( c=0; c<camsize; c++ ))
+                    do
+                        xmlstarlet ed -L -s 'argos-configuration/visualization/qt-opengl/camera' -t elem -n placement -v '' experiments/${EXPFILE}.argos
+                        xmlstarlet ed -L -i "//placement[${c}+1]" -t attr -n idx -v ${c} experiments/${EXPFILE}.argos &&
+                        xmlstarlet ed -L -i "//placement[${c}+1]" -t attr -n position -v ${CAMPOS[${c}]} experiments/${EXPFILE}.argos &&
+                        xmlstarlet ed -L -i "//placement[${c}+1]" -t attr -n look_at -v ${CAMLOOK[${c}]} experiments/${EXPFILE}.argos &&
+                        xmlstarlet ed -L -i "//placement[${c}+1]" -t attr -n lens_focal_length -v ${CAMFOCAL[${c}]} experiments/${EXPFILE}.argos
+                    done
+                fi
+
+                run_experiment
+                ((trial++))
+            done
+        done
+    done
 done
